9c1baf7eca36449575f4a97a9efbd295faf46238
Mahbubul Alam Palash <palashm@leidos.com>
2025-02-14 13:50:27 -0500

    PCISMVP31 Reducing size of status cell

--- src/components/terminal-bookings/columns.tsx
+++ src/components/terminal-bookings/columns.tsx
@@ -54,7 +54,7 @@ export const columns = (status: string): ColumnDef<any>[] => {
   if (status === "Ongoing") {
     baseColumns.push({
       accessorKey: "bookingStatus",
-      header: () => <div className="text-center min-w-[200px]">Status</div>,
+      header: () => <div className="text-center min-w-[150px]">Status</div>,
      // Adjust width as needed
       cell: ({ row }) => {
         const status = row.original.bookingStatus; // Get status value
