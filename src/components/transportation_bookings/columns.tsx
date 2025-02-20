f89385bb20fc7820a53bf06eebb530d11f202079
ChrisC027 <christopher.h.castillo@leidos.com>
2025-02-19 22:31:28 -0500

    updated conditional on disabled modify button

--- src/components/transportation_bookings/columns.tsx
+++ src/components/transportation_bookings/columns.tsx
@@ -398,7 +398,7 @@ export const OngoingColumn = (): ColumnDef<any>[] => {
         return (
           <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
           <DialogTrigger asChild>
-            <Button variant="ghost" className="p-2" onClick={() => setIsDialogOpen(true)} disabled={row.original.containerStatus !== "Pending Pick Up"}>
+            <Button variant="ghost" className="p-2" onClick={() => setIsDialogOpen(true)} disabled={row.original.bookingStatus !== "Pending Pick Up"}>
               <Pencil />
             </Button>
           </DialogTrigger>
