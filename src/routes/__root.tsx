f9ceb3ab45b8354956cca9fe3465c0aa3f3c40a8
ChrisC027 <christopher.h.castillo@leidos.com>
2025-02-24 21:52:17 -0500

    commenting out for testing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,8 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import '../App.css';
-import { useAuthenticator } from '@aws-amplify/ui-react';
-import { fetchUserAttributes } from 'aws-amplify/auth';
-import { useEffect, useState } from 'react';
+// import { useAuthenticator } from '@aws-amplify/ui-react';
+// import { fetchUserAttributes } from 'aws-amplify/auth';
+// import { useEffect, useState } from 'react';
 import { SidebarProvider } from "../components/ui/sidebar";
 import { AppSidebar } from "../components/app-sidebar/app-sidebar"
 // import UserButton from '../components/userButton/userButton';
@@ -16,11 +16,11 @@ import { Toaster } from 'sonner';
 
 // type BookingLimit = SelectionSet<Schema['Limit']['type'], typeof selectionSet>;
 
-interface UserAttributes {
-  given_name?: string;
-  family_name?: string;
-  'custom:role'?: string;
-}
+// interface UserAttributes {
+//   given_name?: string;
+//   family_name?: string;
+//   'custom:role'?: string;
+// }
 
 
 export const Route = createRootRoute({
@@ -37,8 +37,8 @@ export const Route = createRootRoute({
 // const client = generateClient<Schema>();
 
 function RootComponent() {
-  const { user } = useAuthenticator()
-  const [userAttributes, setUserAttributes] = useState<{ fullName: string; role: string }>({ fullName: "", role: "" })
+  // const { user } = useAuthenticator()
+  // const [userAttributes, setUserAttributes] = useState<{ fullName: string; role: string }>({ fullName: "", role: "" })
   // const [bookingLimit, setBookingLimit] = useState<BookingLimit[]>([]);
   
   
@@ -59,29 +59,29 @@ function RootComponent() {
   //   fetchBookingLimit();
   // }, [userAttributes.role]);
 
-  useEffect(() => {
-    const getUserAttributes = async () => {
-      try {
-        const attributes: UserAttributes = await fetchUserAttributes()
+  // useEffect(() => {
+  //   const getUserAttributes = async () => {
+  //     try {
+  //       const attributes: UserAttributes = await fetchUserAttributes()
 
-        const fullName =
-          attributes.given_name && attributes.family_name
-            ? `${attributes.given_name} ${attributes.family_name}`
-            : "Unknown"
+  //       const fullName =
+  //         attributes.given_name && attributes.family_name
+  //           ? `${attributes.given_name} ${attributes.family_name}`
+  //           : "Unknown"
 
-        setUserAttributes({
-          fullName,
-          role: attributes["custom:role"] ?? "No role assigned",
-        })
-      } catch (error) {
-        console.error("Error fetching user attributes:", error)
-      }
-    }
+  //       setUserAttributes({
+  //         fullName,
+  //         role: attributes["custom:role"] ?? "No role assigned",
+  //       })
+  //     } catch (error) {
+  //       console.error("Error fetching user attributes:", error)
+  //     }
+  //   }
 
-    if (user) {
-      getUserAttributes()
-    }
-  }, [user])
+  //   if (user) {
+  //     getUserAttributes()
+  //   }
+  // }, [user])
 
   return (
     <div className="flex min-h-screen bg-background">
