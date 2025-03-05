e5971da27a62ddd5680f9dffdc0438297b121621
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-05 12:41:42 -0500

    modified header

--- src/routes/operators.tsx
+++ src/routes/operators.tsx
@@ -20,6 +20,7 @@ export default function Operators() {
         const response = await fetch(
           "https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com/dev",
           { 
+            method: 'GET',
             headers: {
               "Authorization": `${session.tokens?.idToken?.toString()}`,
               "Content-Type": "application/json"
