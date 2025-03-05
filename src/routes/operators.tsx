c08dbebca6c75717f87b8dd30556877eba047c16
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-05 12:01:49 -0500

    query for table

--- src/routes/operators.tsx
+++ src/routes/operators.tsx
@@ -1,46 +1,31 @@
-"use client"
 import { createFileRoute } from '@tanstack/react-router'
 import { User, columns } from "../components/users/columns"
 import { DataTable } from "../components/users/users-table"
 import { useEffect, useState } from "react"
-import { CognitoIdentityProviderClient, ListUsersCommand } from "@aws-sdk/client-cognito-identity-provider"
+
 
 export const Route = createFileRoute('/operators')({
   component: Operators,
 })
 
-
-export default async function Operators() {
+export default function Operators() {
   const [data, setData] = useState<User[]>([])
   const [loading, setLoading] = useState(true)
 
-  const client = new CognitoIdentityProviderClient({
-    region: 'us-east-1',
-  }); 
-
-  const input = { // ListUsersRequest
-    UserPoolId: "us-east-1_ODcx7VXFP", // required
-    AttributesToGet: [ // SearchedAttributeNamesListType
-      "given_name",
-      "family_name",
-      "email",
-      "phone_number",
-      "custom:organization",
-      "custom:role",
-    ],
-  };
-
-  const command = new ListUsersCommand(input);
-  const response = await client.send(command);
-
   useEffect(() => {
     async function fetchData() {
-      console.log(response)
-      const result = await getData()
-      setData(result)
-      setLoading(false)
+      try {
+        const response = await fetch("https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com/dev");
+        const result = await response.json();
+        console.log(result);
+        setData(result);
+      } catch (error) {
+        throw new Error(`Failed to fetch data: ${error}`);
+      } finally {
+        setLoading(false);
+      }
     }
-    fetchData()
+    fetchData();
   }, [])
 
   if (loading) {
@@ -50,7 +35,7 @@ export default async function Operators() {
   return (
     <div>
       <h1 className="text-2xl font-bold text-center">Available Users</h1>
-      <div>
+      <div className="container mx-auto p-10">
         <DataTable columns={columns} data={data} />
       </div>
     </div>
@@ -58,42 +43,42 @@ export default async function Operators() {
 }
 
 
-async function getData(): Promise<User[]> {
-  // Replace with API Calls
-  return [
-    {
-      name: "John Doe",
-      role: "BCO",
-      organization: "Leidos",
-      email: "johndoe@leidos.com",
-      phone: "123-456-7890",
-      status: "Approved"
-    },
-    {
-      name: "Bob Doe",
-      role: "BCO",
-      organization: "Port Authority NYNJ",
-      email: "bobdoe@panynj.com",
-      phone: "123-456-7890",
-      status: "Approved"
-    },
-    {
-      name: "Adam Smith",
-      role: "BCO",
-      organization: "Port of Virginia",
-      email: "adamsmith@portva.com",
-      phone: "123-456-7890",
-      status: "Approved"
-    },
-    {
-      name: "Jane Wilson",
-      role: "BCO",
-      organization: "CSX",
-      email: "janewilson@csx.com",
-      phone: "123-456-7890",
-      status: "Approved"
-    },
+// async function getData(): Promise<User[]> {
+//   // Replace with API Calls
+//   return [
+//     {
+//       name: "John Doe",
+//       role: "BCO",
+//       organization: "Leidos",
+//       email: "johndoe@leidos.com",
+//       phone: "123-456-7890",
+//       status: "Approved"
+//     },
+//     {
+//       name: "Bob Doe",
+//       role: "BCO",
+//       organization: "Port Authority NYNJ",
+//       email: "bobdoe@panynj.com",
+//       phone: "123-456-7890",
+//       status: "Approved"
+//     },
+//     {
+//       name: "Adam Smith",
+//       role: "BCO",
+//       organization: "Port of Virginia",
+//       email: "adamsmith@portva.com",
+//       phone: "123-456-7890",
+//       status: "Approved"
+//     },
+//     {
+//       name: "Jane Wilson",
+//       role: "BCO",
+//       organization: "CSX",
+//       email: "janewilson@csx.com",
+//       phone: "123-456-7890",
+//       status: "Approved"
+//     },
 
-  ]
-}
+//   ]
+// }
 
