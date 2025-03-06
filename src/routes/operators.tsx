9990e824879cb5d9c5bfb48a7c32765e00d35427
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-06 16:14:18 -0500

    added idtoken

--- src/routes/operators.tsx
+++ src/routes/operators.tsx
@@ -21,9 +21,10 @@ export default function Operators() {
         const response = await fetch(
           "https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com/dev/",
           { 
+            mode: 'no-cors',
             method: 'GET',
             headers: {
-              "Authorization": `Bearer ${session.tokens?.accessToken?.toString()}` ,
+              "Authorization": `Bearer ${session.tokens?.idToken?.toString()}` ,
               "Content-Type": "application/json",
               "Access-Control-Allow-Methods": "GET, OPTIONS",
               "Access-Control-Allow-Origin": "*",
