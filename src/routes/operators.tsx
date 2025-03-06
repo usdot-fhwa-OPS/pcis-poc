558855fbcda64f2ae8304fc5306579acaa2e62f7
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-06 13:02:22 -0500

    simplified api call

--- src/routes/operators.tsx
+++ src/routes/operators.tsx
@@ -19,15 +19,11 @@ export default function Operators() {
         const session = await fetchAuthSession();
         console.log(session.tokens?.idToken?.toString());
         const response = await fetch(
-          "https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com/dev",
+          "https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com/dev/",
           { 
-            mode: 'no-cors',
             method: 'GET',
             headers: {
               "Authorization": session.tokens?.accessToken?.toString() ?? "" ,
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Methods": "GET, OPTIONS",
-              "Accept": "*/*",
             }
           },
         );
