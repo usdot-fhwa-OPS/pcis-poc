4023e9aa38584613ead5e96b46367cfb60cf2c71
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-06 12:39:07 -0500

    modified url

--- src/routes/operators.tsx
+++ src/routes/operators.tsx
@@ -19,7 +19,7 @@ export default function Operators() {
         const session = await fetchAuthSession();
         console.log(session.tokens?.idToken?.toString());
         const response = await fetch(
-          "https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com",
+          "https://xlj2x9eurh.execute-api:us-east-1:286010155551:xlj2x9eurh/*/GET/",
           { 
             mode: 'no-cors',
             method: 'GET',
