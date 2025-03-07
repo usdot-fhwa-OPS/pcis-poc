321975c2d6f4060f4cc8c09e8684ea9bbe8b1d0a
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-07 11:20:53 -0500

    changed to access token

--- src/routes/operators.tsx
+++ src/routes/operators.tsx
@@ -21,7 +21,7 @@ export default function Operators() {
         const response = await fetch("https://xlj2x9eurh.execute-api.us-east-1.amazonaws.com/dev/", {
           method: 'GET',
           headers: {
-            "Authorization": `Bearer ${session.tokens?.idToken?.toString()}`,
+            "Authorization": `Bearer ${session.tokens?.accessToken?.toString()}`,
             "Content-Type": "application/json",
             "Accept": "*/*"
           }
