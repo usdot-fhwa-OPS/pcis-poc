c4317fdca85cab0662c5263348060dd9fd6f1499
ChrisC027 <christopher.h.castillo@leidos.com>
2025-03-13 22:31:05 -0400

    moved toast

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -83,7 +83,7 @@ function RootComponent() {
       <SidebarProvider>
         <AppSidebar />
         <div className="flex-1">
-          <Toaster position="bottom-right" richColors={true} />
+          <Toaster position="top-center" richColors={true} expand={true} />
           <div className="flex items-center justify-end p-4">
             <UserButton fullName={userAttributes.fullName} role={userAttributes.role} limit={bookingLimit ?? 0} />
           </div>
