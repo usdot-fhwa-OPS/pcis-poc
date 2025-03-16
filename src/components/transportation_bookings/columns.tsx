44ceb01c1c91991c794e02a8f83fd0fc911c7ff9
Mahbubul Alam Palash <palashm@leidos.com>
2025-03-16 00:37:18 -0400

    correcting color code of red

--- src/components/transportation_bookings/columns.tsx
+++ src/components/transportation_bookings/columns.tsx
@@ -166,7 +166,7 @@ export const CompletedColumn = (): ColumnDef<any>[] => {
         const status = row.original.bookingStatus; // Get status value
         const isLate = status === "Late"; // Check if status is "Late"
         return (
-          <span className={`px-2 py-1 font-bold rounded-full bg-gray-300 ${isLate ? "text-red" : "text-black"} text-center inline-block`}>
+          <span className={`px-2 py-1 font-bold rounded-full bg-gray-300 ${isLate ? "text-red-500" : "text-black"} text-center inline-block`}>
             {status}
           </span>
         );
