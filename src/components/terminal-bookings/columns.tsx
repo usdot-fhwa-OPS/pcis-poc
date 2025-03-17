37e246e9284972513a1775ac9bafa7fa815b5b0f
Mahbubul Alam Palash <palashm@leidos.com>
2025-03-17 14:00:05 -0400

    centering Terminal Operator table header

--- src/components/terminal-bookings/columns.tsx
+++ src/components/terminal-bookings/columns.tsx
@@ -11,15 +11,16 @@ const client = generateClient<Schema>();
 export const columns = (status: string): ColumnDef<any>[] => {
   const baseColumns: ColumnDef<any>[] = [
 
-    { accessorKey: "vesselID", header: "Vessel ID" },
-    { accessorKey: "containerID", header: "Container ID" },
-    { accessorKey: "origin", header: "Origin" },
-    { accessorKey: "bcoName", header: "BCO" },
-    { accessorKey: "bcoEmail", header: "BCO Email" },
-    { accessorKey: "transopName", header: "Transportation Operator" },
-    { accessorKey: "transopEmail", header: "Transportation Operator Email" },
-    { accessorKey: "bookingDate", header: "Original Date Requested" },
-    { accessorKey: "bookingTime", header: "Original Time Requested" },
+    { accessorKey: "vesselID", header: () => <div className="text-center">Vessel ID</div> },
+    { accessorKey: "containerID", header: () => <div className="text-center">Container ID</div> },
+    { accessorKey: "origin", header: () => <div className="text-center">Origin</div> },
+    { accessorKey: "bcoName", header: () => <div className="text-center">BCO</div> },
+    { accessorKey: "bcoEmail", header: () => <div className="text-center">BCO Email</div> },
+    { accessorKey: "transopName", header: () => <div className="text-center">Transportation Operator</div> },
+    { accessorKey: "transopEmail", header: () => <div className="text-center">Transportation Operator Email</div> },
+    { accessorKey: "bookingDate", header: () => <div className="text-center">Original Date Requested</div> },
+    { accessorKey: "bookingTime", header: () => <div className="text-center">Original Time Requested</div> },
+  
 
   ];
 
