9c2db45b428958de3e8f19013ec106eb256f397b
Mahbubul Alam Palash <palashm@leidos.com>
2025-03-17 12:39:26 -0400

    Giving a fixed size to status

--- src/components/transportation_bookings/columns.tsx
+++ src/components/transportation_bookings/columns.tsx
@@ -204,7 +204,7 @@ export const OngoingColumn = (): ColumnDef<any>[] => {
     { accessorKey: "bookingTime", header: "Time Initiated" },
     {
       accessorKey: "status",
-      header: () => <div className="min-w-[150px]">Reservation Status</div>,
+      header: () => <div className="w-[150px] text-center ">Reservation Status</div>,
       cell: ({ row, table }) => {
 
         const [date, setDate] = useState<Date | undefined>(new Date())
@@ -363,7 +363,7 @@ export const OngoingColumn = (): ColumnDef<any>[] => {
           </Dialog>
         ) : (
           <span
-          className={`px-2 py-1 font-bold text-center inline-flex items-center justify-center rounded-md bg-gray-300 ${
+          className={`px-2 py-1 font-bold items-center justify-center text-center whitespace-normal w-[150px] break-words rounded-md bg-gray-300 ${
             row.original.bookingStatus === "Late for Pick Up" ? "text-red-600" : "text-black"
           }`}
         >
