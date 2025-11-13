f88fca2712626ec13aa7b76e636736a0d189119f
ChrisC027 <christopher.h.castillo@leidos.com>
2025-11-13 14:13:17 -0500

    n

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -47,7 +47,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
+    defaultAuthorizationMode: 'userPool',
   },
 });
 
