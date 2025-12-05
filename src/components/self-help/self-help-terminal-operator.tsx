ad216ae0fdf3ed29df8377235de696a47b200d21
Ranjitkar <ranjitkaa@leidos.com>
2025-12-05 08:33:07 -0500

    updated with Icons

--- src/components/self-help/self-help-terminal-operator.tsx
+++ src/components/self-help/self-help-terminal-operator.tsx
@@ -1,7 +1,7 @@
 import { Button } from "../ui/button"
 import { Popover, PopoverClose, PopoverContent, PopoverTrigger } from "../ui/popover"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "../ui/accordion"
-import { X } from "lucide-react"
+import { Flag, Ship, SquareChartGantt, User, X } from "lucide-react"
 
 export const TerminalOperatorSelfHelp = () => {
     return (
@@ -10,13 +10,11 @@ export const TerminalOperatorSelfHelp = () => {
                 <Button variant="outline">Self Help</Button>
             </PopoverTrigger>
             <PopoverContent className="w-80">
-                <div><PopoverClose
+                <PopoverClose
 					className="absolute right-[5px] top-[5px] inline-flex size-[25px] cursor-default items-center justify-center rounded-full text-violet11 outline-none hover:bg-violet4 focus:shadow-[0_0_0_2px] focus:shadow-violet7"
 					aria-label="Close">
 					<X />
-				</PopoverClose></div>
-                <div></div>
-                <div></div>
+				</PopoverClose>
                 <Accordion
                     type="single"
                     collapsible
@@ -24,25 +22,25 @@ export const TerminalOperatorSelfHelp = () => {
                     
                 >
                     <AccordionItem value="item-1">
-                        <AccordionTrigger>Where can I manage users?</AccordionTrigger>
+                        <AccordionTrigger><User />Where can I manage users?</AccordionTrigger>
                         <AccordionContent className="flex flex-col gap-4 text-balance">
                             Where can I manage users?
                         </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="item-2">
-                        <AccordionTrigger>Where can I find my upconming cargo?</AccordionTrigger>
+                        <AccordionTrigger><Ship />Where can I find my upconming cargo?</AccordionTrigger>
                         <AccordionContent className="flex flex-col gap-4 text-balance">
                             Where can I find my upconming cargo?
                         </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="item-3">
-                        <AccordionTrigger>How can I flag the upcoming carge?</AccordionTrigger>
+                        <AccordionTrigger><Flag />How can I flag the upcoming carge?</AccordionTrigger>
                         <AccordionContent className="flex flex-col gap-4 text-balance">
                             How can I flag the upcoming carge?
                         </AccordionContent>
                     </AccordionItem>
                     <AccordionItem value="item-4">
-                        <AccordionTrigger>How can I manage my profile?</AccordionTrigger>
+                        <AccordionTrigger><SquareChartGantt />How can I manage my profile?</AccordionTrigger>
                         <AccordionContent className="flex flex-col gap-4 text-balance">
                             How can I manage my profile?
                         </AccordionContent>
