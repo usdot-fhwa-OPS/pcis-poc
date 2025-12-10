0858bfdeeebbe062929c076929d12e85ee5dff7b
ashim-leidos <ashimr31@yahoo.com>
2025-12-10 15:40:53 -0500

    Update self-help-beneficiary-cargo-owner.tsx

--- src/components/self-help/self-help-beneficiary-cargo-owner.tsx
+++ src/components/self-help/self-help-beneficiary-cargo-owner.tsx
@@ -21,7 +21,6 @@ export const BeneficiaryCargoOwnerSelfHelp = () => {
                         type="single"
                         collapsible
                         className="w-full"
-                        defaultValue="item-1"
                     >
                         <AccordionItem value="item-1">
 
