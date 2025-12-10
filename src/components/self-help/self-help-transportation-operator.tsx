13bc99d9749381b622085bae89c6c2effce2114b
ashim-leidos <ashimr31@yahoo.com>
2025-12-10 15:40:38 -0500

    Update self-help-transportation-operator.tsx

--- src/components/self-help/self-help-transportation-operator.tsx
+++ src/components/self-help/self-help-transportation-operator.tsx
@@ -19,7 +19,6 @@ export const TransportationOperatorSelfHelp = () => {
                         type="single"
                         collapsible
                         className="w-full"
-                        defaultValue="item-1"
                     >
                         <AccordionItem value="item-1">
                             <AccordionTrigger>
