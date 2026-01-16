783c5fbef3c182b21d4db44c1f291536afcbf5ae
Radder <radderk@leidos.com>
2026-01-16 13:32:38 -0500

    revision for transportation_bookings

--- src/components/transportation_bookings/columns.tsx
+++ src/components/transportation_bookings/columns.tsx
@@ -321,7 +321,8 @@ export const OngoingColumn = (): ColumnDef<any>[] => {
               <DialogHeader>
                 <DialogTitle>Reserve Container Pick-Up</DialogTitle>
                 <div className="text-sm text-muted-foreground">
-                  {`Vessel ID: ${row.original.vesselID} | Cargo Unit ID: ${row.original.cargoUnitID} | Origin: ${row.original.origin} | BCO: ${row.original.bcoName} | BCO Email: ${row.original.bcoEmail}`}  /* changed containerID and Container ID to cargoUnitID and Cargo unit ID */
+                  {/* changed containerID and Container ID to cargoUnitID and Cargo unit ID */}
+                  {`Vessel ID: ${row.original.vesselID} | Cargo Unit ID: ${row.original.cargoUnitID} | Origin: ${row.original.origin} | BCO: ${row.original.bcoName} | BCO Email: ${row.original.bcoEmail}`}  
                 </div>
               </DialogHeader>
                 <div className="grid gap-4 py-4">
