0266cfcdab559345f4dfa942accef5b6485422d4
Dave Morris <david.p.morris.jr@leidos.com>
2026-02-09 09:53:41 -0500

    tweak css

--- src/components/terminal-capacity/update-terminal-capacity-button.tsx
+++ src/components/terminal-capacity/update-terminal-capacity-button.tsx
@@ -71,7 +71,7 @@ export function MaximumTerminalCapacityButton({ role, limit}: SettingsDialogProp
                               setPortCapacity(Number(e.target.value))
                            }
                            step="1"
-                           className="five-chars"
+                           className="three-chars"
                     />
                      &nbsp;reservations per day
                   </div>
