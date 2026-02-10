0cb95d40e7604c507f62388af2f01ae255be4249
Dave Morris <david.p.morris.jr@leidos.com>
2026-02-10 07:59:12 -0500

    bug fix

--- src/components/terminal-capacity/delete-terminal-capacity-button.tsx
+++ src/components/terminal-capacity/delete-terminal-capacity-button.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useState } from "react"
 import { Settings } from "lucide-react"
 import { Button } from "../ui/button"
 import {
