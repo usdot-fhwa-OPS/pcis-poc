ab3630ca68605e3cae62f316c27bd7c588ef2a93
Ranjitkar <ranjitkaa@leidos.com>
2026-02-23 13:15:21 -0500

    more incoporation of PR feedback

--- src/components/terminal-capacity/update-terminal-capacity.tsx
+++ src/components/terminal-capacity/update-terminal-capacity.tsx
@@ -163,7 +163,7 @@ const [endTime, setEndTime] = useState<string | undefined>(timeOptions[0])
                 <div className="grid grid-cols-5 gap-2">
                     <div className="h-10 col-span-3 col-start-1 ...">
                         <DialogHeader>
-                            <DialogTitle>Add Temporrary Capicity</DialogTitle>
+                            <DialogTitle>Add Temporrary Capacity</DialogTitle>
                             <DialogDescription></DialogDescription>
                         </DialogHeader>
                     </div>
