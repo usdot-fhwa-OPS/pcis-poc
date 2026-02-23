225810bf7bf131bf1c55cdfb492e4d4c85aa3c20
Dave Morris <david.p.morris.jr@leidos.com>
2026-02-23 13:00:14 -0500

    create berth sidebar item

--- src/components/app-sidebar/app-sidebar.tsx
+++ src/components/app-sidebar/app-sidebar.tsx
@@ -1,4 +1,4 @@
-import { FileUp, Home, Ship, User , CalendarClock, BarChart } from "lucide-react"
+import { FileUp, Home, Ship, User , CalendarClock, BarChart, Anchor } from "lucide-react"
 
 import {
   Sidebar,
@@ -57,6 +57,11 @@ const items = [
     title: "Reservation Status",
     url: "/reservation",
     icon: CalendarClock,
+  },
+  {
+    title: "Berth Reservation",
+    url: "/berth",
+    icon: Anchor,
   },
   {
     title: "Analytics",
