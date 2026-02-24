89caf34fa5b6585d090e65f3dcf130f29556a49a
AndrewF70 <andrew.b.fortier@leidos.com>
2026-02-24 08:18:53 -0500

    Feature/terminal capacity table (#65)
    * Add terminal capacity table component
    
    * Remove filter search bar from terminal capacity table
    
    * Add delete button and update button functionality to terminal capacity table
    
    * Correction to remove directory that is no longer needed
    
    * Remove role property from update and delete button files
    
    * Correction to implementation of Delete and Update buttons
    
    * Update to rendering of Delete and Update buttons
    
    * Formatting update to text before Update button
    
    * Correction to src/routes/index.tsx file
    
    * Remove parenthesis from Update button and add spaces to text before button

---
+++ src/components/terminal_capacity_table/data-table.tsx
@@ -0,0 +1,76 @@
+"use client"
+import * as React from "react"
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+  ColumnFiltersState,
+  getFilteredRowModel,
+} from "@tanstack/react-table"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../ui/table.tsx"
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[]
+  data: TData[]
+}
+
+export function DataTable<TData, TValue>({ columns, data} : DataTableProps<TData, TValue>) {
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    state: { columnFilters },
+  })
+
+  return (
+    
+    <div className="mb-4 w-xl max-w-9/10 px-3 py-2 border rounded-md">
+
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  )
+}
--- src/routes/capacity.tsx
+++ src/routes/capacity.tsx
@@ -1,13 +1,65 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { type Capacity, columns } from "../components/terminal_capacity_table/columns"
+import { DataTable } from "../components/terminal_capacity_table/data-table"
+import { UpdateTerminalCapacityButton } from "../components/terminal-capacity/update-terminal-capacity-button.tsx";
+import { useEffect, useState } from "react"
 
 export const Route = createFileRoute('/capacity')({
-  component: RouteComponent,
+  component: Capacity
 })
 
-function RouteComponent() {
+
+export default function Capacity() {
+  const [data, setData] = useState<Capacity[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await getData()
+      setData(result)
+      setLoading(false)
+    }
+    fetchData()
+  }, [])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+
   return (
     <div className="p-2">
       <h1 className="text-2xl font-bold text-center">Terminal Capacity</h1>
+      <p className="text-left">Maximum Terminal Capacity: 5 Reservations per day&nbsp;&nbsp;
+      <UpdateTerminalCapacityButton limit={5}/>
+      </p>
+      <div className="container mx-auto p-10">
+              <DataTable
+                columns={columns()}
+                data={data}
+              />
+            </div>
     </div>
+    
   )
+}
+
+async function getData() {
+  // Replace with API Calls
+  return [
+    {
+      capacity : 3,
+      startTime: "12/12/2025 9:30 AM",
+      endTime: "12/12/2025 3:30 PM",
+      repeat: "Never",
+      reason: "Equipment Malfunction"
+    },
+    {
+      capacity : 4,
+      startTime: "12/15/2025 6:00 AM",
+      endTime: "12/15/2025 6:00 PM",
+      repeat: "Never",
+      reason: "Labor Shortage"
+    }
+  ]
 }
