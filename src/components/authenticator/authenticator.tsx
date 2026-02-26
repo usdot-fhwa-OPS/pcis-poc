1cbed5373f541fe91adb6f900c7efa1f5657bf80
Radder <radderk@leidos.com>
2026-02-26 11:13:14 -0500

    adding vessel agent organization

--- src/components/authenticator/authenticator.tsx
+++ src/components/authenticator/authenticator.tsx
@@ -52,8 +52,8 @@ const orgOptions = [
 	<option key="Illinois and Midland Railroad (IMRR)"	value="Illinois and Midland Railroad (IMRR)">Illinois and Midland Railroad (IMRR)</option>,
 	<option key="ABC Drayage"						value="ABC Drayage">ABC Drayage</option>,
 	<option key="XYZ 3PL"								value="XYZ 3PL">XYZ 3PL</option>,
-	<option key="Leidos" 								value="Leidos">Leidos</option>
-  <option key="Vessel Agent XYZ" 								value="Vessel Agent XYZ">Vessel Agent XYZ</option>
+	<option key="Leidos" 								value="Leidos">Leidos</option>,
+  <option key="Vessel Agent XYZ" 								value="Vessel Agent XYZ">Vessel Agent XYZ</option>,
   <option key="Vessel Agent ABC" 								value="Vessel Agent ABC">Vessel Agent ABC</option>
 
 ];
