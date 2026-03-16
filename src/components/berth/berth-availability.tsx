78b632c03abed883777bced03396696f636ee9f9
Dave Morris <david.p.morris.jr@leidos.com>
2026-03-16 13:24:18 -0400

    formatting

--- src/components/berth/berth-availability.tsx
+++ src/components/berth/berth-availability.tsx
@@ -38,11 +38,11 @@ export const BerthAvailability = ({limit}: SettingsDialogProps) => {
                                     setBerthAvailability(Number(e.target.value))
                                 }
                                 step="1"
-                                className="four-chars"/>
+                                className="w-4"/>
                     </div>
                     <br/>
                     <br/>
-                    <div className="ml-35 flex items-center gap-2">
+                    <div className="ml-35 flex items-right gap-2">
                         <Button type="button" variant="outline" onClick={() => setIsDialogOpen(false)}>
                                 Cancel
                         </Button>
