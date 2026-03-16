761c18f6ac11c852dedd1596b8052b7fc41328c2
Dave Morris <david.p.morris.jr@leidos.com>
2026-03-16 11:25:49 -0400

    more detail, added buttons

--- src/components/berth/berth-availability.tsx
+++ src/components/berth/berth-availability.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 //import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "../ui/dialog";
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog";
-
+import { Button } from "../ui/button";
 
 export const BerthAvailability = () => {
 
@@ -15,8 +15,15 @@ export const BerthAvailability = () => {
           <DialogHeader className="p-4 border-b">
             <div className="flex items-center gap-2">
               <h2 className="text-lg font-semibold">Berth Availability</h2>
+            </div>
+            <div>  
               <h3 className="text-sm">Set the maximum number of berths available at the terminal.</h3>
             </div>
+            <div>
+                Berths Available:
+            </div>
+            <Button>Cancel</Button>
+            <Button>Enter Berth Availability</Button>
           </DialogHeader>
         </DialogContent>
       </Dialog>
