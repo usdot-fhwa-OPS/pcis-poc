8b9d8cf3b9fcad139c6c86c2b19c9ec9e5879c0a
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-03-24 17:29:22 -0400

    Adding basic framework for Add New Berth Request button.

--- src/routes/berth-requests.tsx
+++ src/routes/berth-requests.tsx
@@ -7,7 +7,12 @@ export const Route = createFileRoute('/berth-requests')({
 function RouteComponent() {
   return (
     <div className="p-2">
-      <h1 className="text-2xl font-bold text-center">Berth Requests</h1>
+      <h1 className="text-2xl font-bold">Berth Requests</h1>
+    </div>
+    <div className="container mx-auto p-10">
+      <div className="parent-container">
+        <button type="button">Add New Berth Request</button>
+      </div>
     </div>
   )
 }
