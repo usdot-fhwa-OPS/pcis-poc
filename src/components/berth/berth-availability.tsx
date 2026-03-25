7bcc4542858c331f2571af85dc321746c52c287f
Dave Morris <david.p.morris.jr@leidos.com>
2026-03-25 12:59:03 -0400

    merge berth availability set berths

--- src/components/berth/berth-availability.tsx
+++ src/components/berth/berth-availability.tsx
@@ -28,6 +28,8 @@ export const BerthAvailability = () => {
 
     const [isDialogOpen, setIsDialogOpen] = useState(true)
 
+    const [berthAvailability, setBerthAvailability] = useState(limit)
+
     const handleSubmit = async () => {
     }
   
@@ -43,39 +45,50 @@ export const BerthAvailability = () => {
                             <h3 className="text-sm">Enter the names of the terminal's berth below.</h3>
                         </div>
                     </DialogHeader>
+                    <div className="ml-4 flex gap-2">
+                        <Label htmlFor="berthsAvailable">
+                            Berths Available:
+                        </Label>
+*                         <input id="berthAvailability" type="number" min={0} max={limit} value={berthAvailability}
+                                onChange={(e) =>
+                                    setBerthAvailability(Number(e.target.value))
+                                }
+                                step="1"
+                                className="w-8"/> 
+                    </div>                    
                     <div className="space-y-3">
-                    {berths.map((value, index) => (
-                        <div key={index} className="ml-4 flex items-center gap-2">
-                        <Label className="w-12">Berth {index + 1}</Label>
-                        <input
-                            type="text"
-                            value={value}
-                            maxLength={30}
-                            onChange={(e) => updateBerth(index, e.target.value)}
-                            className="border px-2 py-1 w-[30ch]" />
+                        {berths.map((value, index) => (
+                            <div key={index} className="ml-4 flex items-center gap-2">
+                            <Label className="w-12">Berth {index + 1}</Label>
+                            <input
+                                type="text"
+                                value={value}
+                                maxLength={30}
+                                onChange={(e) => updateBerth(index, e.target.value)}
+                                className="border px-2 py-1 w-[30ch]" />
 
-                        {index === berths.length - 1 && berths.length < limit ? (
-                            <button type="button"
-                            onClick={addBerth}
-                            className="px-2 py-1 bg-green-500 text-white rounded"
-                            >
-                            Add
-                            </button>
-                        ) : (
-                            <button type="button"
-                            onClick={() => removeBerth(index)}
-                            disabled={berths.length === 1}
-                            className={`px-2 py-1 rounded text-white ${
-                               berths.length === 1
-                                ? "bg-gray-400 cursor-not-allowed"
-                                : "bg-red-500"
-                            }`}
-                            >
-                            Remove
-                            </button>
-                        )}
-                        </div>
-                    ))}
+                            {index === berths.length - 1 && berths.length < limit ? (
+                                <button type="button"
+                                onClick={addBerth}
+                                className="px-2 py-1 bg-green-500 text-white rounded"
+                                >
+                                Add
+                                </button>
+                            ) : (
+                                <button type="button"
+                                onClick={() => removeBerth(index)}
+                                disabled={berths.length === 1}
+                                className={`px-2 py-1 rounded text-white ${
+                                berths.length === 1
+                                    ? "bg-gray-400 cursor-not-allowed"
+                                    : "bg-red-500"
+                                }`}
+                                >
+                                Remove
+                                </button>
+                            )}
+                            </div>
+                        ))}
                     </div>
                     <br/>
                     <br/>
