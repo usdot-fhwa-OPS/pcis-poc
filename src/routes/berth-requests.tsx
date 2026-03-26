88d608c34372d3c29a964fa27fd9e22b1ea0ba9d
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-03-26 13:27:53 -0400

    Fixed issues adding button to page.

--- src/routes/berth-requests.tsx
+++ src/routes/berth-requests.tsx
@@ -1,13 +1,20 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Button } from "../components/ui/button";
+
 export const Route = createFileRoute('/berth-requests')({
-  component: RouteComponent,
+  component: BerthRequestComponent,
 })
 
-function RouteComponent() {
+function BerthRequestComponent() {
   return (
+  <>
     <div className="p-2">
       <h1 className="text-2xl font-bold text-center">Berth Requests</h1>
     </div>
+    <div className="p-2">
+        <Button className="justlify-end">Add New Berth Request</Button>
+    </div>
+  </>
   )
 }
