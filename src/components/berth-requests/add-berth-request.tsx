c970d54bf76080ed21ed63fdeb7be568b98966d3
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-03-30 17:28:01 -0400

    Fix to address another deployment error.

--- src/components/berth-requests/add-berth-request.tsx
+++ src/components/berth-requests/add-berth-request.tsx
@@ -1,3 +1,5 @@
+"use client"
+import React, { useState } from "react";
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import { CalendarIcon } from "lucide-react";
