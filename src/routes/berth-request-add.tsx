681778a23f496f0cb09f9b6e5ffd104dcf44b632
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-03-30 11:26:57 -0400

    Adding component file for Berth Request form

--- src/routes/berth-request-add.tsx
+++ src/routes/berth-request-add.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { AddBerthRequest } from "../components/berth-requests/add-berth-request";
+
 export const Route = createFileRoute('/berth-request-add')({
   component: AddBerthRequestComponent,
 })
@@ -8,8 +10,8 @@ function AddBerthRequestComponent() {
   return (
   <>
     <div className="flex flex-col w-full p-10">
-        <h1 className="text-2xl font-semibold">Add New Berth Request</h1>
-        <p><em>Insert form</em></p>
+        <h1 className="text-2xl font-semibold mb-6">Add New Berth Request</h1>
+        <AddBerthRequest/>
     </div>
   </>
   )
