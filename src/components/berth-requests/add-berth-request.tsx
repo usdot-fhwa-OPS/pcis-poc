627d2e40d0cd1ed32a2a4de95139fd83de99e2e7
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-03-31 10:42:10 -0400

    Fix to address deployment error.

--- src/components/berth-requests/add-berth-request.tsx
+++ src/components/berth-requests/add-berth-request.tsx
@@ -1,5 +1,4 @@
-"use client"
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "../ui/button";
 import { Calendar } from "../ui/calendar";
 import { CalendarIcon } from "lucide-react";
