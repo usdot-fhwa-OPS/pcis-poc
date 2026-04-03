c7ac7f93b1a094b5746d809e58ae0386225fdfff
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-04-03 12:03:12 -0400

    Adding function to Cancel button on Add New Berth Request page to take user back to Berth Requests page.

--- src/routes/berth-request-add.tsx
+++ src/routes/berth-request-add.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, useNavigate } from '@tanstack/react-router'
 
 import { Button } from "../components/ui/button";
 import { AddBerthRequest } from "../components/berth-requests/add-berth-request";
@@ -8,13 +8,19 @@ export const Route = createFileRoute('/berth-request-add')({
 })
 
 function AddBerthRequestComponent() {
+  
+  const navigate = useNavigate();
+  const navigateToBerthRequests = () => {
+    navigate({ to: "/berth-requests" });
+  }
+  
   return (
   <>
     <div className="flex flex-col w-full p-10">
         <h1 className="text-2xl font-semibold mb-6">Add New Berth Request</h1>
         <AddBerthRequest/>
         <div className="flex flex-col-reverse gap-2 sm:flex-row sm:justify-start sm:gap-0 sm:space-x-4 mt-6">
-          <Button variant="outline">
+          <Button variant="outline" onClick={navigateToBerthRequests}>
             Cancel
           </Button>
           <Button>
