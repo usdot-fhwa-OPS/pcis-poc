38b5ddebf33713eea923d3fa699f7d924d90d137
Dave Morris <david.p.morris.jr@leidos.com>
2026-04-08 08:11:10 -0400

    updated berth manager for open dialog

--- src/routes/berth-manager.tsx
+++ src/routes/berth-manager.tsx
@@ -4,12 +4,17 @@ import { BerthAvailability } from '../components/berth/berth-availability.tsx'
 export const Route = createFileRoute('/berth-manager')({
   component: RouteComponent,
 })
+    const handleOpenDialog = () => {
+      <BerthAvailability></BerthAvailability>
+    }
 
 function RouteComponent() {
   return (
     <div className="p-2">
       <h1 className="text-2xl font-bold text-center">Berth Reservations</h1>
-      <BerthAvailability></BerthAvailability>
+      <button onClick={handleOpenDialog}>
+          Berth Availability
+      </button>      
     </div>
   )
 }
