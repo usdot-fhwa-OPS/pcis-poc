c07c7eb9decef75699a400d07ae26e77fb37e126
Dave Morris <david.p.morris.jr@leidos.com>
2026-04-13 11:44:26 -0400

    Initial commit

--- src/routes/berth-manager.tsx
+++ src/routes/berth-manager.tsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState } from "react"
 import { createFileRoute } from "@tanstack/react-router"
 import { BerthAvailability } from "../components/berth/berth-availability"
 import { Button } from "../components/ui/button"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs.tsx"
 
 export const Route = createFileRoute('/berth-manager')({
   component: RouteComponent,
@@ -14,7 +15,31 @@ function RouteComponent() {
         setIsOpen(!isOpen);
       };
 
-      return (
+     return (
+      <div className="w-xl max-w-9/10">
+        <Tabs defaultValue="requested" className="">
+          <div>
+        <TabsList className="mb-4 flex w-full justify-start gap-x-4">
+            <TabsTrigger value="requested">Requested</TabsTrigger>
+            <TabsTrigger value="modification">Modification Requested</TabsTrigger>
+            <TabsTrigger value="ongoing">Ongoing</TabsTrigger>
+            <TabsTrigger value="completed">Completed</TabsTrigger>
+          </TabsList>
+          </div>
+          <div className="w-xl max-w-9/10">
+          <TabsContent value="requested">
+          </TabsContent>
+          <TabsContent value="modification">
+           </TabsContent>
+          <TabsContent value="ongoing">
+          </TabsContent>
+          <TabsContent value="completed">
+          </TabsContent>
+          </div>
+        </Tabs>
+      </div>
+    );
+/*       return (
         <div className="p-2">
           <h1 className="text-2xl font-bold text-center">Berth Reservations</h1>
           <Button type="button" onClick={() => handleOpen()}>
@@ -22,5 +47,5 @@ function RouteComponent() {
           </Button>
           <BerthAvailability isDialogOpen={isOpen} handleCloseDialog={() => setIsOpen(false)} />
         </div>
-    )
+    ) */
 }
