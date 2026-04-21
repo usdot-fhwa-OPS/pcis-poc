74abcec935ef02eb653c467d9011c996dedd27cd
Dave Morris <david.p.morris.jr@leidos.com>
2026-04-21 16:24:12 -0400

    restored sample data

--- src/routes/berth-manager.tsx
+++ src/routes/berth-manager.tsx
@@ -13,7 +13,7 @@ export const Route = createFileRoute('/reservation')({
   component: RouteComponent,
 })
 
-export const sampleBerthRequests: BerthRequest[] = [/* 
+export const sampleBerthRequests: BerthRequest[] = [ 
   {
     id: "BR-1001",
     vesselId: "VSL-7782",
@@ -148,7 +148,7 @@ export const sampleBerthRequests: BerthRequest[] = [/*
     eta: "2026-05-02T06:40:00Z",
     etd: "2026-05-03T17:20:00Z",
     requestedAt: "2026-04-19T13:25:00Z",
-  },   */
+  },   
 ];
 
 function RouteComponent() {
@@ -182,7 +182,7 @@ function RouteComponent() {
     getUserAttributes();
   }, [user]);
 
-  if (userAttributes.role === "Terminal Operator") {
+  if (userAttributes.role === "Terminal Operator" || userAttributes.role === "Vessel Agent") {
     return (
       <div className="w-xl max-w-9/10">
         <div className="flex items-center">
