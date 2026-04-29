922c16021465f32d100f56c249b60cac2d5c00c3
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-04-29 09:50:37 -0400

    Fixing build error. Added undefined arguments to onclick function. Removed twicEscortRequired console logs.

--- src/components/reservations/approve-reservation.tsx
+++ src/components/reservations/approve-reservation.tsx
@@ -121,8 +121,7 @@ export function ApproveReservation({vesselId, cargoId, origin, bcoName, bcoEmail
                     <Button
                         className="bg-green-600 hover:bg-green-600/90"
                         onClick={() =>  {
-                            (dataTableMeta as TerminalOperatorDataTableMeta)?.updateBooking(cargoId, "Pending Pick Up", selectedValue); 
-                            (dataTableMeta as TerminalOperatorDataTableMeta)?.updateTwicEscortRequired(cargoId, selectedValue);
+                            (dataTableMeta as TerminalOperatorDataTableMeta)?.updateBooking(cargoId, "Pending Pick Up", undefined, undefined, selectedValue); 
                         }}
                     >
                         Yes, Approve
--- src/routes/reservation.tsx
+++ src/routes/reservation.tsx
@@ -642,9 +642,6 @@ async function updateBooking(id: string, status: string, reservationDate?: strin
     console.log("Updated booking status:", updatedContainerStatus);
     toast.success("Booking status updated successfully");
 
-    const { data: updatedTwicRequirement } = await client.models.Container.update({twicEscortRequired});
-    console.log("Updated TWIC escort requirement :", updatedTwicRequirement);
-
     // Refresh relevant data after successful update
     await fetchterminal_operator_requested();
     await fetchTerminalOperatorModified();
