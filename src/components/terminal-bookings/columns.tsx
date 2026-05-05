6a22ac6360f80a990f68f183a4161986daa4bae1
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-05-05 12:54:27 -0400

    Fixing build error. Restructuring table header scripting.

--- src/components/terminal-bookings/columns.tsx
+++ src/components/terminal-bookings/columns.tsx
@@ -155,15 +155,21 @@ export const columns = (status: string): ColumnDef<any>[] => {
       enableColumnFilter: false,
     },
     {
-      header: () => <div className="min-w-[180px]"><abbr className="decoration-[1px] decoration-dotted underline-offset-4" title="Transportation Worker Identification Credential">TWIC</abbr></div>,
+      header: () => {
+        return (
+          <abbr className="decoration-[1px] decoration-dotted underline-offset-4" title="Transportation Worker Identification Credential">TWIC</abbr>
+        );
+      },
       cell: () => {
         return (
-          <Badge 
-            className="border-amber-200 rounded-full bg-amber-100 text-amber-700 hover:bg-amber-100/80 gap-1"
-          >
-            <ShieldHalf fill="#fff" className="inline-block w-4 h-4" />
-            TWIC Escort Required
-          </Badge>
+          <div className="min-w-[180px]">
+            <Badge 
+              className="border-amber-200 rounded-full bg-amber-100 text-amber-700 hover:bg-amber-100/80 gap-1"
+            >
+              <ShieldHalf fill="#fff" className="inline-block w-4 h-4" />
+              TWIC Escort Required
+            </Badge>
+          </div>
         );
       },
     });
