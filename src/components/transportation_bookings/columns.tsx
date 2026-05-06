37f4a1ca12e9ccd525a5134dfec8607df7164c97
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-05-06 14:26:43 -0400

    Added TWIC column and TWIC Escort Required badge to transportation operator's Ongoing reservations table.

--- src/components/transportation_bookings/columns.tsx
+++ src/components/transportation_bookings/columns.tsx
@@ -89,6 +89,26 @@ export const columns = (): ColumnDef<any>[] => {
       
     },
     {
+      accessorKey: "twicEscortRequired",
+      header: () => <abbr className="decoration-[1px] decoration-dotted underline-offset-4" title="Transportation Worker Identification Credential">TWIC</abbr>,
+      cell: ({ row }) => {
+        const required = row.original.twicEscortRequired; // Get TWIC value
+        
+        if (required === true) {
+          return (
+            <div className="min-w-[164px]">
+              <Badge 
+                className="border-amber-200 rounded-full bg-amber-100 text-amber-700 hover:bg-amber-100/80 gap-1"
+              >
+                <ShieldHalf fill="#fff" className="inline-block w-4 h-4" />
+                TWIC Escort Required
+              </Badge>
+            </div>
+          );
+        }
+      }
+    },
+    {
       accessorKey: "contact_bco",
       header: "Contact BCO",
       cell: ({ row }) => {
