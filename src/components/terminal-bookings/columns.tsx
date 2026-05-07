a6a59489b7d1145f6b11caec1f4f70d655be0e7d
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-05-07 14:07:52 -0400

    Added svg containing gradient fill to TWIC table header in Completed table.

--- src/components/terminal-bookings/columns.tsx
+++ src/components/terminal-bookings/columns.tsx
@@ -295,7 +295,15 @@ export const CompletedColumn = (): ColumnDef<any>[] => {
     },
     {
       accessorKey: "twicEscortRequired",
-      header: () => <abbr className="decoration-[1px] decoration-dotted underline-offset-4" title="Transportation Worker Identification Credential">TWIC</abbr>,
+      header: () => <div>
+          <abbr className="decoration-[1px] decoration-dotted underline-offset-4" title="Transportation Worker Identification Credential">TWIC</abbr>
+          <svg width="0" height="0">
+            <linearGradient id="shieldhalf-icon-gradient" x1="0%" y1="0%" x2="100%" y2="0%">
+              <stop stopColor="#ff6467" offset="50%" />
+              <stop stopColor="#ffffff" offset="50%" />
+            </linearGradient>
+          </svg>
+        </div>, // Includes svg gradient fill for ShieldHalf icon
       cell: ({ row }) => {
         const required = row.original.twicEscortRequired; // Get TWIC value
         
