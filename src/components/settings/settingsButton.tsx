594765845d422c91ca6e22800b268e82e367e749
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-05-28 14:25:04 -0400

    Added temporary Notifications and Messages icon buttons to user-button to see how full header row will look once all features work. Note: moving notifications from the sidebar to the header row may need to be its own story.

--- src/components/settings/settingsButton.tsx
+++ src/components/settings/settingsButton.tsx
@@ -23,7 +23,7 @@ export default function SettingsButton({ role}: SettingsDialogProps) {
     return (
       <Button onClick={navigateToCapacity} variant="ghost" size="icon" className="[&_svg]:size-5">
         <Settings />
-        <span className="sr-only">Open settings</span>
+        <span className="sr-only">Open Settings</span>
       </Button>
      )
   }  
--- src/components/userButton/userButton.tsx
+++ src/components/userButton/userButton.tsx
@@ -1,7 +1,9 @@
 
 import { Avatar, AvatarFallback } from "../ui/avatar";
+import { Button } from "../ui/button";
 import { Selfhelp } from '../self-help/self-help';
 import SettingsButton from '../settings/settingsButton';
+import { Bell, Mail } from "lucide-react"
 import './userButtonStyles.css';
 
 interface UserButtonProps {
@@ -13,6 +15,14 @@ const UserButton: React.FC<UserButtonProps> = ({ fullName, role }) => {
     return (
     <>
         <Selfhelp />
+        <Button variant="ghost" size="icon" className="[&_svg]:size-5">
+            <Mail />
+            <span className="sr-only">Open Messages</span>
+        </Button>
+        <Button variant="ghost" size="icon" className="[&_svg]:size-5">
+            <Bell />
+            <span className="sr-only">Open Notifications</span>
+        </Button>
         <SettingsButton role={role} />
         <div className="user-button">
             <div className="user-button-content">
