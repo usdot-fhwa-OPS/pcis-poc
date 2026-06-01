0d20b3a6423627835387cbef2ae8a82c8b3e1c48
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-01 16:03:43 -0400

    Responsive adjustments to header's right padding and column gap below 640px wide

--- src/components/user-header/userHeader.tsx
+++ src/components/user-header/userHeader.tsx
@@ -36,7 +36,7 @@ const UserHeader: React.FC<UserHeaderProps> = ({ fullName, role }) => {
     
     return (
     <>
-        <div className="flex items-center justify-end gap-5 p-3 pr-6 bg-white border-b mb-4">
+        <div className="flex items-center justify-end gap-4 sm:gap-5 p-3 sm:pr-6 bg-white border-b mb-4">
             <Selfhelp />
             <Button variant="ghost" size="icon" className="gap-[0.125rem] w-[2.875rem] [&_svg]:size-5">
                 <Mail />
