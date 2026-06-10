97b9132d22497dc6112738c0a288a33d949999b4
ChrisC027 <christopher.h.castillo@leidos.com>
2026-06-10 09:49:26 -0400

    reverting centering

--- src/components/terminal_capacity_table/columns.tsx
+++ src/components/terminal_capacity_table/columns.tsx
@@ -9,9 +9,6 @@ export const columns: ColumnDef<TerminalCapacityDomain>[] = [
   {
     accessorKey: "capacity",
     header: "Capacity",
-    cell: ({ row }) => (
-      <div className="text-center">{row.original.capacity}</div>
-    ),
   },
   {
     id: "startDateTime",
