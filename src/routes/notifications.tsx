7aeadcb17741178f18a8022ae8bebf60c3b4cbfa
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-11 13:16:51 -0400

    Basic padding and page title styling for standalone page.

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -6,8 +6,8 @@ export const Route = createFileRoute('/notifications')({
 
 function RouteComponent() {
   return (
-    <div className="p-2">
-      <h3>Notification Page</h3>
+    <div className="p-4 pt-3 pl-[4.125rem]">
+      <h3 className="text-2xl font-semibold text-gray-900">Notifications</h3>
     </div>
   )
 }
