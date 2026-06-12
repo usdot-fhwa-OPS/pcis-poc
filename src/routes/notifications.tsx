c567471fd80155914a8710f321b25bf2117f7079
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-12 16:28:22 -0400

    Refining page styling.

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -8,12 +8,12 @@ export const Route = createFileRoute('/notifications')({
 
 function RouteComponent() {
   return (
-    <div className="p-4 pt-3 pl-16">
+    <div className="pt-2 px-16 pb-16">
       <h1 className="flex items-center gap-2 text-2xl font-semibold text-gray-900">
         <Bell className="w-6 h-6 text-gray-700" />
         Notifications
       </h1>
-      <div className="mt-[3.75rem] pb-12">
+      <div className="mt-[3.75rem]">
         <p>Notifications will display here.</p>
       </div>
     </div>
