6f06c16c2097acb73f8bf59d7b67b4b218f4302a
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-12 15:41:43 -0400

    Added bell icon to page title, set up divs and spacing for notifications content.

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { Bell } from "lucide-react"
+
 export const Route = createFileRoute('/notifications')({
   component: RouteComponent,
 })
@@ -7,7 +9,13 @@ export const Route = createFileRoute('/notifications')({
 function RouteComponent() {
   return (
     <div className="p-4 pt-3 pl-[4.125rem]">
-      <h3 className="text-2xl font-semibold text-gray-900">Notifications</h3>
+      <div className="flex items-center gap-2">
+        <Bell className="w-6 h-6 text-gray-700" />
+        <h1 className="text-2xl font-semibold text-gray-900">Notifications</h1>
+      </div>
+      <div className="mt-[3.75rem] mb-15">
+        <p>Notifications will display here.</p>
+      </div>
     </div>
   )
 }
