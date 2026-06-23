a133aab12758a06ce11d406c96d8ebe568b26e29
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-23 17:31:53 -0400

    Dialing back colors of test icons.

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -269,8 +269,8 @@ function RouteComponent() {
         )
       })}
         <div className="flex items-center gap-4 bg-white p-4 border mb-2 last:mb-0 rounded-xl shadow">
-            <span className="inline-flex shrink-0 rounded-full p-3 bg-blue-100">
-              <CalendarCheck className="size-4 stroke-blue-700" />
+            <span className="inline-flex shrink-0 rounded-full p-3 bg-blue-50">
+              <CalendarCheck className="size-4 stroke-blue-600" />
             </span>
             <span className="inline-flex shrink-0 rounded-full p-3 bg-amber-50">
               <Settings className="size-4 stroke-amber-600" />
@@ -278,8 +278,8 @@ function RouteComponent() {
             <span className="inline-flex shrink-0 rounded-full p-3 bg-red-50">
               <TriangleAlert className="size-4 stroke-red-600" />
             </span>
-            <span className="inline-flex shrink-0 rounded-full p-3 bg-gray-100">
-              <Info className="size-4 stroke-gray-700" />
+            <span className="inline-flex shrink-0 rounded-full p-3 bg-gray-50">
+              <Info className="size-4 stroke-gray-600" />
             </span>
         </div>
       </div>
