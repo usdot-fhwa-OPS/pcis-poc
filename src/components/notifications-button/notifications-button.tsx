00bcf94ed5ff1c60606968ce720e60e702f7aab1
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-24 17:40:13 -0400

    Fixing build error. Removing call for Notifications function.

--- src/components/notifications-button/notifications-button.tsx
+++ src/components/notifications-button/notifications-button.tsx
@@ -5,7 +5,6 @@ import { useState } from "react"
 import { Dialog, DialogContent, DialogHeader } from "../ui/dialog"
 import { ScrollArea } from "../ui/scroll-area"
 import { SidebarMenuItem, SidebarMenuButton, SidebarMenuBadge } from "../ui/sidebar"
-import { Notifications } from "../app-sidebar/app-sidebar"
 import { Link } from "@tanstack/react-router"
 import { format } from "date-fns"
 
