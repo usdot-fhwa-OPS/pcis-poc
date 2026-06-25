1b32eebe1df768da4874190ef13f2934fbb682d7
ChrisC027 <christopher.h.castillo@leidos.com>
2026-06-25 08:35:00 -0400

    Header change

--- src/routes/berth-requests.tsx
+++ src/routes/berth-requests.tsx
@@ -17,7 +17,7 @@ export function BerthRequestComponent() {
   <>
     <div className="flex flex-col w-full px-6 pt-6 md:px-10 md:pt-8">
       <div className="flex items-center justify-between mb-4">
-        <h1 className="text-2xl font-bold text-gray-900">Berth Requests</h1>
+        <h1 className="text-2xl font-bold text-gray-900">Berth Reservations</h1>
         {AddNewBerthRequestButton()}
       </div>
     </div>
