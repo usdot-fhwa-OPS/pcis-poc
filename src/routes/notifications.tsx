ef39eb27f213e3ada28306122f2d1c5de7051a48
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-06-26 16:30:59 -0400

    Updated Notifications page to use the useNotifications hook. Also fixed a minor issue with the key property in the timestamp scripting, it was originally using notification.id and should have been using notification.cargoUnitID.

--- src/routes/notifications.tsx
+++ src/routes/notifications.tsx
@@ -3,20 +3,10 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Badge } from "../components/ui/badge";
 import { Bell, CalendarCheck, Clock, Info, Settings, TriangleAlert } from "lucide-react"
 
-import { fetchUserAttributes } from 'aws-amplify/auth';
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import { useEffect, useState } from "react";
-
-import { generateClient, SelectionSet } from 'aws-amplify/data';
-import type { Schema } from '../../amplify/data/resource';
-import { Subscription } from "rxjs";
-
 import { Link } from "@tanstack/react-router"
 import { format, formatDistanceToNow, isAfter, subHours } from "date-fns"
 
-const client = generateClient<Schema>();
-const selectionSet = ['cargoUnitID', 'reservationStatus', "updatedAt", "isBCONotify", "isTransportationNotify"] as const; 
-export type Notifications = SelectionSet<Schema['Container']['type'], typeof selectionSet> 
+import { useNotifications, Notifications } from '../hooks/useNotifications';
 
 export const Route = createFileRoute('/notifications')({
   component: RouteComponent,
@@ -24,95 +14,7 @@ export const Route = createFileRoute('/notifications')({
 
 function RouteComponent() {
 
-  const { user } = useAuthenticator();
-
-  const [userAttributes, setUserAttributes] = useState<{ role: string; email: string }>({
-    role: '',
-    email: '',
-  });
-
-  const [userNotifications, setUserNotifications] = useState<Notifications[]>([]);
-
-  useEffect(() => {
-    async function getUserAttributes() {
-      if (user) {
-        try {
-          const attributes = await fetchUserAttributes();
-          const roleAttribute = attributes['custom:role'] ?? 'No role assigned';
-          setUserAttributes({
-            role: roleAttribute,
-            email: attributes.email ?? 'No email found',
-          });
-        } catch (error) {
-          console.error('Error fetching user attributes', error);
-        }
-      }
-    }
-
-    getUserAttributes();
-  }, [user]);
-
-
-  const [refresh, setRefresh] = useState(0);
-
-  // Subscribe to updates and trigger refresh.
-  useEffect(() => {
-    const updateSubscription = client.models.Container.onUpdate().subscribe({
-      next: () => {
-        // Increment the refresh counter to trigger re-running the observeQuery.
-        setRefresh((prev) => prev + 1);
-      },
-      error: (error) => console.warn(error),
-    });
-    return () => updateSubscription.unsubscribe();
-  }, []);
-
-  // Subscribe to notifications based on user attributes and refresh state.
-  useEffect(() => {
-    if (!userAttributes.role) return;
-    let notisSub: Subscription;
-
-    if (userAttributes.role === "Beneficiary Cargo Owner") {
-      notisSub = client.models.Container.observeQuery({
-        filter: {
-          and: [
-            { bcoEmail: { eq: userAttributes.email } },
-            { isBCONotify: { eq: true } },
-          ],
-        },
-      }).subscribe({
-        next: ({ items }) => {
-          setUserNotifications(items);
-        },
-      });
-    } else if ((userAttributes.role === 'Trucking Operator') 
-          || (userAttributes.role === 'Rail Operator')
-          || (userAttributes.role === 'Third Party Logistics Provider')) {
-      notisSub = client.models.Container.observeQuery({
-        filter: {
-          isTransportationNotify: { eq: true },
-        },
-      }).subscribe({
-        next: ({ items }) => {
-          setUserNotifications(items);
-        },
-      });
-    } else if (userAttributes.role === 'Terminal Operator') {
-      notisSub = client.models.Container.observeQuery({
-        filter: {
-          isTerminalNotify: { eq: true },
-        },
-      }).subscribe({
-        next: ({ items }) => {
-          setUserNotifications(items);
-        },
-      });
-    }
-
-    return () => {
-      notisSub.unsubscribe();
-    };
-  }, [userAttributes, refresh]);
+  const { userNotifications, userAttributes } = useNotifications();
 
   const notificationCount = userNotifications.length;
   const unreadNotificationCount = 1;
@@ -270,7 +172,7 @@ function RouteComponent() {
             </div>
             <div className="ml-auto self-start flex items-center gap-1 text-xs text-muted-foreground text-nowrap leading-none">
               <Clock className="w-3 h-3" />
-              <span key={notification.id}>{displayTime}</span>
+              <span key={notification.cargoUnitID}>{displayTime}</span>
             </div>
           </div>
           )
