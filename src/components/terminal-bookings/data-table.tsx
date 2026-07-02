ed10d900245e6739dbef2ae0a31f411ccd7f9337
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-07-02 15:39:50 -0400

    Styling header row.

--- src/components/terminal-bookings/data-table.tsx
+++ src/components/terminal-bookings/data-table.tsx
@@ -58,7 +58,7 @@ export function DataTable<TData, TValue>({ columns, data ,meta}: DataTableProps<
       <Table>
         <TableHeader>
           {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
+            <TableRow key={headerGroup.id} className="bg-gray-50 hover:bg-gray-50/50 data-[state=selected]:bg-gray-50">
               {headerGroup.headers.map((header) => (
                 <TableHead key={header.id}>
                   {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
