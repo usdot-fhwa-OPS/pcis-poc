c978527a31a7767b83d58709f2a028ed8439aba0
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-07-02 14:43:01 -0400

    Fixing build error. (For real this time.) Forgot to remove variant property from TabsList last time.

--- src/routes/reservation.tsx
+++ src/routes/reservation.tsx
@@ -707,7 +707,7 @@ useEffect(() => {
       <div className="w-xl max-w-9/10 px-6 py-6 md:px-10 md:py-8">
         <h1 className="mb-4 text-2xl leading-4 font-semibold text-gray-900">Cargo Reservations</h1>
         <Tabs defaultValue="requested">
-          <TabsList variant="line" className="h-auto mb-4 p-0 bg-transparent gap-x-4">
+          <TabsList className="h-auto mb-4 p-0 bg-transparent gap-x-4">
             <TabsTrigger value="requested" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900">Requested</TabsTrigger>
             <TabsTrigger value="modification" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900">Modification Requested</TabsTrigger>
             <TabsTrigger value="ongoing" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900">Ongoing</TabsTrigger>
