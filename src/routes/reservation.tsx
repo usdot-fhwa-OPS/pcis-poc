bceef8f3af01af8f06193506a6b10110ec178352
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-07-02 15:14:51 -0400

    More styling for tabs, they're still displaying a background tone. Adjusting page title's margin to match up with prototype.

--- src/routes/reservation.tsx
+++ src/routes/reservation.tsx
@@ -705,13 +705,13 @@ useEffect(() => {
   if (userAttributes.role === "Terminal Operator") {
     return (
       <div className="w-xl max-w-9/10 px-6 py-6 md:px-10 md:py-8">
-        <h1 className="mb-4 text-2xl leading-4 font-semibold text-gray-900">Cargo Reservations</h1>
+        <h1 className="mb-6 text-2xl leading-4 font-semibold text-gray-900">Cargo Reservations</h1>
         <Tabs defaultValue="requested">
           <TabsList className="h-auto mb-4 p-0 bg-transparent gap-x-4">
-            <TabsTrigger value="requested" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Requested</TabsTrigger>
-            <TabsTrigger value="modification" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Modification Requested</TabsTrigger>
-            <TabsTrigger value="ongoing" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Ongoing</TabsTrigger>
-            <TabsTrigger value="completed" className="pt-0 px-0 pb-2 data-[state=active]:border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Completed</TabsTrigger>
+            <TabsTrigger value="requested" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Requested</TabsTrigger>
+            <TabsTrigger value="modification" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Modification Requested</TabsTrigger>
+            <TabsTrigger value="ongoing" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Ongoing</TabsTrigger>
+            <TabsTrigger value="completed" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Completed</TabsTrigger>
           </TabsList>
           <div className="w-xl max-w-9/10">
             <TabsContent value="requested" className="mt-0">
