3502ca256e6a0570b2409302945024fbce85cf67
Ranjitkar <ranjitkaa@leidos.com>
2026-07-13 21:52:57 -0400

    - added dynamo database table to persist websocket connection IDs - added ability to reconnect after connecion is closed

--- lambdas/cargo-unit-sync/index.mjs
+++ lambdas/cargo-unit-sync/index.mjs
@@ -2,57 +2,75 @@ import {
   ApiGatewayManagementApiClient,
   PostToConnectionCommand,
 } from "@aws-sdk/client-apigatewaymanagementapi";
+import { DynamoDBClient, ScanCommand, PutItemCommand, DeleteItemCommand } from "@aws-sdk/client-dynamodb";
+const dynamo = new DynamoDBClient({});
+
 
 const CARGO_UNITS_TABLE = process.env.CARGO_UNITS_TABLE || "CargoUnits";
-const connectIdList = [];
-const realTimeEventNotify = async(message) =>{
+const WEBSOCKET_CONNECTIONS_TABLE = process.env.CARGO_UNITS_TABLE || "PcisWebsocketConn"
+const realTimeEventNotify = async (message) => {
 
   const callbackUrl = `https://n7w79iw8p7.execute-api.us-east-1.amazonaws.com/dev/`;
   const client = new ApiGatewayManagementApiClient({ endpoint: callbackUrl });
-  console.log(connectIdList)
-  for (const id of connectIdList) {
+  const scanData = await dynamo.send(new ScanCommand({ TableName: WEBSOCKET_CONNECTIONS_TABLE }));
+  const connections = scanData.Items.map(item => item.connectionId.S);
+
+  console.log(connections)
+  connections.map(async (connectionId) => {
     try {
 
       const requestParams = {
-        ConnectionId: id,
+        ConnectionId: connectionId,
         Data: message,
       };
 
       const command = new PostToConnectionCommand(requestParams);
+      console.log(`sending notification connection ID ${connectionId} ${message}`)
+      await client.send(command)
+      
+      // await fetch(`${callbackUrl}/@connections/${connectionId}`, {
+      //       method: 'POST',
+      //       body: message,
+            
+      //   });
 
-      await client.send(command);
-      console.log(`notified connection ID ${id} ${message}`)
+      console.log(`notified connection ID ${connectionId} ${message}`)
+
     } catch (error) {
       console.log(error);
+      await removeWebSocketConnection(connectionId);
     }
 
-  }
+  });
 
 
 }
 
 
-export const handler = async(event, context) => {
-  console.log(event )
-   
+export const handler = async (event, context) => {
+  console.log(event)
+
 
   //  //for websocket connect and disconnet events
-    if(event["requestContext"]){
-      const connectId = event["requestContext"]["connectionId"]
-      const domainName = event["requestContext"]["domainName"]
-      const stageName = event["requestContext"]["stage"]
-      const qs = event['queryStringParameters']
-       console.log('Connection ID: ', connectId, 'Domain Name: ', domainName, 'Stage Name: ', stageName, 'Query Strings: ', qs )
-          const eventType = event["requestContext"]["eventType"];
-      if('CONNECT' === eventType){
+  if (event["requestContext"]) {
+    const connectionId = event["requestContext"]["connectionId"]
+    const domainName = event["requestContext"]["domainName"]
+    const stageName = event["requestContext"]["stage"]
+    const qs = event['queryStringParameters']
+    console.log('Connection ID: ', connectionId, 'Domain Name: ', domainName, 'Stage Name: ', stageName, 'Query Strings: ', qs)
+    const eventType = event["requestContext"]["eventType"];
+    if ('CONNECT' === eventType) {
 
-        connectIdList.push(connectId);
+      await dynamo.send(new PutItemCommand({
+        TableName: WEBSOCKET_CONNECTIONS_TABLE,
+        Item: { connectionId: { S: connectionId } }
+      }));
 
-      }else if('DISCONNECT' === eventType){
+    } else if ('DISCONNECT' === eventType) {
 
-        connectIdList.splice(connectIdList.indexOf(connectId), 1);
-      }
+      await removeWebSocketConnection(connectionId);
     }
+  }
 
   // for database trigger  
   if (event.Records) {
@@ -62,5 +80,12 @@ export const handler = async(event, context) => {
 
   }
 
-  return {"statusCode" : 200}
+  return { "statusCode": 200 }
 };
+
+async function removeWebSocketConnection(connectionId) {
+  await dynamo.send(new DeleteItemCommand({
+    TableName: WEBSOCKET_CONNECTIONS_TABLE,
+    Key: { connectionId: { S: connectionId } }
+  }));
+}
--- src/components/real-time-call.tsx
+++ src/components/real-time-call.tsx
@@ -7,21 +7,35 @@ export const onCargoUpdate = onCargoUpdate$.asObservable();
 const onCargoCreate$ = new BehaviorSubject('');
 export const onCargoCreate = onCargoCreate$.asObservable();
 
-const websocket = new WebSocket('wss://n7w79iw8p7.execute-api.us-east-1.amazonaws.com/dev/');
-websocket.onopen = () => 
-        console.log('Connected to WebSocket server');
-      websocket.onmessage = (event) => {
-        console.log('Message received ' + event.data);
-        if ('INSERT CargoUnits' === event.data) {
-          onCargoCreate$.next(event.data)
-        } else if ('MODIFY CargoUnits' === event.data) {
-          onCargoUpdate$.next(event.data)
-        }
+const url = 'wss://n7w79iw8p7.execute-api.us-east-1.amazonaws.com/dev/';
+let websocket:WebSocket;
+const connect = () => {
+  websocket = new WebSocket(url);
+  websocket.onopen = () =>
+    console.log('Connected to WebSocket server');
+  websocket.onmessage = (event) => {
+    console.log('Message received ' + event.data);
+    if ('INSERT CargoUnits' === event.data) {
+      onCargoCreate$.next(event.data)
+    } else if ('MODIFY CargoUnits' === event.data) {
+      onCargoUpdate$.next(event.data)
+    }
 
-      };
-      websocket.onclose = () => 
-        console.log('Disconnected from WebSocket server');
+  };
+  websocket.onclose = () => {
+    console.log('Disconnected from WebSocket server');
+    setTimeout(() => {
+      console.log('Reconnecting WebSocket server');
+      connect();
 
+    }, 1000 * 2);
+
+  }
+
+}
+
+connect();
+      
 export const sendMessage = (msg: string) => {
       if (websocket && websocket.readyState === WebSocket.OPEN) {
         websocket.send(msg);
