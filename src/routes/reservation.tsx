e0a7d8cba5ee920843f18eacd3e833059a3f68d5
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-07-21 16:40:57 -0400

    Reworking spacing and layout of tabs, missed that classes were necessary.

--- src/routes/reservation.tsx
+++ src/routes/reservation.tsx
@@ -706,11 +706,11 @@ useEffect(() => {
       <div className="w-full px-6 py-6 md:px-10 md:py-8">
         <h1 className="mb-6 text-2xl leading-4 font-semibold text-gray-900">Cargo Reservations</h1>
         <Tabs defaultValue="requested">
-          <TabsList className="w-full mb-4 gap-x-6 border-b border-gray-200">
-            <TabsTrigger value="requested" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Requested</TabsTrigger>
-            <TabsTrigger value="modification" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Modification Requested</TabsTrigger>
-            <TabsTrigger value="ongoing" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Ongoing</TabsTrigger>
-            <TabsTrigger value="completed" className="pt-0 px-0 pb-2 data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Completed</TabsTrigger>
+          <TabsList className="justify-start gap-x-6 w-full h-auto p-0 mb-4 bg-transparent border-b border-gray-200">
+            <TabsTrigger value="requested" className="pt-0 pb-2 data-[state=active]:-mb-px data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Requested</TabsTrigger>
+            <TabsTrigger value="modification" className="pt-0 pb-2 data-[state=active]:-mb-px data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Modification Requested</TabsTrigger>
+            <TabsTrigger value="ongoing" className="pt-0 pb-2 data-[state=active]:-mb-px data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Ongoing</TabsTrigger>
+            <TabsTrigger value="completed" className="pt-0 pb-2 data-[state=active]:-mb-px data-[state=active]:bg-transparent border-b-2 border-transparent data-[state=active]:border-gray-900 rounded-none text-gray-500 hover:text-gray-700 data-[state=active]:text-gray-900 shadow-none data-[state=active]:shadow-none">Completed</TabsTrigger>
           </TabsList>
           <TabsContent value="requested">
             <TerminalBookingsTable data={terminalopBookingsupcoming} status="Requested" meta={{updateBooking}} />
