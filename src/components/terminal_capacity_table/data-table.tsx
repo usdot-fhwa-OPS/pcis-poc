462d79869e0a9b0dba2e5fec2a5a6634e9bb59a4
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-07-28 01:33:36 -0400

    Responsive adjustments for text and button area under the page title.

--- src/components/terminal_capacity_table/data-table.tsx
+++ src/components/terminal_capacity_table/data-table.tsx
@@ -96,7 +96,7 @@ export function DataTable<TData, TValue>({
         </Table>
       </div>
       {/* Pagination */}
-      <div className="flex flex-row max-sm:flex-col items-center justify-between max-sm:justify-center gap-y-0 max-sm:gap-y-4 px-4 py-3 border-t text-sm text-gray-600">
+      <div className="flex max-sm:flex-col items-center justify-between max-sm:justify-center max-sm:gap-y-4 px-4 py-3 border-t text-sm text-gray-600">
         <span>{firstRow} - {lastRow} of {totalRows} items</span>
         <div className="flex items-center gap-1">
           <button
--- src/routes/capacity.tsx
+++ src/routes/capacity.tsx
@@ -42,15 +42,16 @@ export default function TerminalCapacityComponent() {
    <div className="w-full px-6 py-6 md:px-10 md:py-8">
       <h1 className="mb-6 text-2xl leading-none font-semibold text-gray-900">Capacity Planning</h1>
       <div className="flex items-center justify-between mb-4">
-        <p className="text-sm text-gray-700">
-          Maximum Terminal Capacity:&nbsp;
-          <span className="font-semibold">{maxTerminalCapacity[0].capacity} Reservations per day</span>
-          &nbsp;&nbsp;
-          <UpdateTerminalCapacityButton maxTerminalCapacity={maxTerminalCapacity[0]}/>
+        <p className="flex max-sm:flex-col text-sm text-gray-700">
+          <span className="pr-2">Maximum Terminal Capacity:</span>
+          <span className="font-medium">
+            {maxTerminalCapacity[0].capacity} Reservations per day
+            <UpdateTerminalCapacityButton maxTerminalCapacity={maxTerminalCapacity[0]} />
+          </span>
         </p>
         <AddTerminalCapacity/>
       </div>
-      <TerminalCapacityTable data={data.filter((item)=>(item.capacityType==='TEMPORARY'))}/>
+      <TerminalCapacityTable data={data.filter((item)=>(item.capacityType==='TEMPORARY'))} />
     </div>
   )
 
