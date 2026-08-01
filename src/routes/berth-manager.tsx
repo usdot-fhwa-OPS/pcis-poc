a5f0237304258769e19276878c12748c27cb0f01
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-08-01 03:03:59 -0400

    Adjusting bottom margin of header to  adjust for 8px of extra height added by the button.

--- src/routes/berth-manager.tsx
+++ src/routes/berth-manager.tsx
@@ -60,7 +60,7 @@ function RouteComponent() {
 
   return (
     <div className="w-full px-6 py-6 md:pt-6 md:px-10 md:pb-8">
-      <div className="flex max-sm:flex-col max-sm:gap-y-4 items-center max-sm:items-start justify-between mb-6">
+      <div className="flex max-sm:flex-col max-sm:gap-y-4 items-center max-sm:items-start justify-between mb-4 max-sm:mb-6">
         <h1 className="text-2xl leading-none font-semibold text-gray-900">Berth Reservations</h1>
         <Button 
           variant="outline" 
