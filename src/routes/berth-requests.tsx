c16d63ca3c1b76c1aaedb0197ac4a7e9c8160b32
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-08-04 11:29:00 -0400

    Updated Berth Reservations page for Vessel Agents.

--- src/routes/berth-requests.tsx
+++ src/routes/berth-requests.tsx
@@ -15,9 +15,9 @@ export function BerthRequestComponent() {
   
   return (
   <>
-    <div className="flex flex-col w-full px-6 pt-6 md:px-10 md:pt-8">
-      <div className="flex items-center justify-between mb-4">
-        <h1 className="text-2xl font-bold text-gray-900">Berth Reservations</h1>
+    <div className="w-full px-6 py-6 md:pt-6 md:px-10 md:pb-8">
+      <div className="flex max-sm:flex-col max-sm:gap-y-4 items-center max-sm:items-start justify-between mb-4 max-sm:mb-6">
+        <h1 className="text-2xl leading-none font-semibold text-gray-900">Berth Reservations</h1>
         {AddNewBerthRequestButton()}
       </div>
     </div>
