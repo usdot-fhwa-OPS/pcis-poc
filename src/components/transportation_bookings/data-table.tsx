eef7f6564bbbab6c6f29f8c9a117d46be6e3268c
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-08-04 10:52:28 -0400

    Minor updates to Cargo Reservations table for Transportation Coordinator.

--- src/components/transportation_bookings/data-table.tsx
+++ src/components/transportation_bookings/data-table.tsx
@@ -74,7 +74,7 @@ export function DataTable<TData, TValue>({ columns, data, meta }: DataTableProps
       <div className="relative ml-auto">
         <Search className="absolute left-2.5 top-1/2 -translate-y-1/2 h-4 w-4 text-gray-400 pointer-events-none" />
         <Input
-          placeholder="Filter by cargo unit ID" 
+          placeholder="Search cargo unit ID" 
           value={(table.getColumn("cargoUnitID")?.getFilterValue() as string) ?? ""} 
           onChange={(event) =>
             table.getColumn("cargoUnitID")?.setFilterValue(event.target.value) 
