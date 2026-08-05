20f2d747aee926f6875851d8276b273631f221db
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-08-05 15:16:26 -0400

    Quick style adjustments to Berth Reservations table for Terminal Manager.

--- src/components/berth-requests/terminal-manager-table/data-table.tsx
+++ src/components/berth-requests/terminal-manager-table/data-table.tsx
@@ -108,7 +108,7 @@ export function DataTable<TData, TValue>({
                   className="hover:bg-gray-50/50 data-[state=selected]:bg-gray-50/50"
                 >
                   {row.getVisibleCells().map((cell) => (
-                    <TableCell key={cell.id} className="p-4">
+                    <TableCell key={cell.id} className="p-4 has-[button]:px-4 has-[button]:py-3">
                       {flexRender(cell.column.columnDef.cell, cell.getContext())}
                     </TableCell>
                   ))}
