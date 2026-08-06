f3aefd95e865d77adf5ba13b26360d24096a1dc5
Sean Duffey <Sean.C.Duffey@leidos.com>
2026-08-06 03:08:45 -0400

    Quick styling corrections.

--- src/routes/import.tsx
+++ src/routes/import.tsx
@@ -11,17 +11,17 @@ function RouteComponent() {
   return (
     <div className="w-full px-6 py-6 md:px-10 md:py-8">
 
-      <h1 className="mb-2 text-2xl leading-none font-semibold text-gray-900">Upload Manifest</h1>
-      <p className="mb-4 text-sm text-gray-700">Upload vessel manifest or Manifest &mdash; CSV format supported.</p>
-      <p className="mb-4font-medium text-gray-900">Instructions for file attachment:</p>
-      <ol className="mb-8 space-y-4 list-decimal list-inside text-sm">
+      <h1 className="mb-2 text-2xl leading-none font-bold text-gray-900">Upload Manifest</h1>
+      <p className="mb-6 text-sm text-gray-700">Upload vessel manifest or Manifest &mdash; CSV format supported.</p>
+      <p className="mb-2 font-medium text-gray-900">Instructions for file attachment:</p>
+      <ol className="mb-8 space-y-2 list-decimal list-inside text-sm">
         <li>Locate your cargo ma nifest file on your computer.</li>
         <li>Drag and drop the file into the upload area below or select &ldquo;Browse Files&rdquo; to find it.</li>
         <li>A check mark will appear next to your file&apos;s name when it is uploaded.</li>
       </ol>
 
       <div className="mb-4 p-6 bg-white border border-gray-200 rounded-xl">
-        <h2 className="mb-2 text-lg font-medium text-gray-900">Upload Manifest File</h2>
+        <h2 className="mb-2 text-lg leading-none font-medium text-gray-900">Upload Manifest File</h2>
         <p className="flex max-sm:flex-col gap-x-2 mb-8 text-sm text-gray-500">
           <span>Accepted format: .csv</span> <span className="max-sm:hidden font-bold">&#128900;</span> <span>This file contains: cargo unit IDs, vessel info, origin/destination, arrival timing</span>
         </p>
@@ -35,11 +35,11 @@ function RouteComponent() {
         />
       </div>
       <div className="mb-4 p-6 bg-white border border-gray-200 rounded-xl">
-        <h3 className="mb-2 text-md font-medium text-gray-900">Required Data Fields</h3>
-        <p className="mb-4 pb-2 border border-bottom text-sm text-gray-500">The manifest CSV must include the following columns:</p>
+        <h3 className="mb-2 text-md leading-none font-medium text-gray-900">Required Data Fields</h3>
+        <p className="mb-4 pb-1 border-b text-sm text-gray-500">The manifest CSV must include the following columns:</p>
         <p>Bullet points here.</p>
       </div>
-      <div className="flex justify-end gap-2">
+      <div className="flex justify-end gap-3">
         <Button variant="outline">Cancel</Button>
         <Button variant="default">Submit Manifest</Button>
       </div>
